1f52ef88d93d81fe6811c1aca9e0036efead99c6
Haze Booth <isnt@haze.cool>
2019-06-13 00:25:06 -0400

    Add Rust

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+
+/// caesar represents the caesar cipher function, shifting the characters by `n` spaces.
+pub fn caesar(source: &str, n: usize) -> String {
+    // calculate cipher so we DP only up until N=alphabet
+    let alphabet = "abcdefguhijklmnopqrstuvwxyz";
+    let upper_alphabet = alphabet.to_uppercase();
+
+    let upper_alphabet_raw = upper_alphabet.as_bytes();
+    let alphabet_raw = alphabet.as_bytes();
+
+    let alphabet_len = alphabet.len();
+    let source_len = source.len();
+    let cipher = {
+        let mut map = HashMap::with_capacity(alphabet_len * 2);
+        for (i, ch) in alphabet
+            .chars()
+            .enumerate()
+            .chain(upper_alphabet.chars().enumerate())
+            {
+                let cipher_char = {
+                    let offset = (i + n) % alphabet_len;
+                    if ch.is_uppercase() {
+                        upper_alphabet_raw[offset]
+                    } else {
+                        alphabet_raw[offset]
+                    }
+                };
+                map.insert(ch as char, cipher_char as char);
+            }
+        map
+    };
+    let mut output = String::with_capacity(source_len);
+    let raw_source = source.as_bytes();
+    for (i, ch) in source.chars().enumerate() {
+        let original = raw_source[i] as char;
+        output.push(*cipher.get(&ch).unwrap_or_else(|| &original));
+    }
+    output
+}
+
+mod test {
+    use crate::caesar;
+
+    #[test]
+    fn caesar_cipher_works() {
+        assert_eq!(caesar("hello, world", 0), String::from("hello, world"));
+        assert_eq!(caesar("hello world", 1), String::from("ifmmp xpsme"));
+    }
+}
